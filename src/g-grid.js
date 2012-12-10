ebd8c19d67df8de287a8bfbc2e28a0e6e4312a89
aw2basc <a@aw2basc.com>
2012-12-10 02:04:26 -0600

    g-grid

--- src/g-grid.js
+++ src/g-grid.js
@@ -1,117 +1,223 @@
 var fs = require('fs'),
-	toWrite = '',
+	gGrid = '',
 	grids = {
 wuxga : {
 	grid:1848,
 	short:'h',
 	long:'hd',
-	res:'WUXGA'
+	res:'WUXGA',
+	module:54,
+	gutter:24,
+	padding:25,
+	columns:24
 },
 wxga : {
 	grid:1314,
 	short:'w',
 	long:'wide',
-	res:'WXGA'
+	res:'WXGA',
+	module:54,
+	gutter:30,
+	padding:15,
+	columns:16
 },
 xga : {
 	grid:978,
 	short:'d',
 	long:'desktop',
-	res:'XGA'
+	res:'XGA',
+	module:54,
+	gutter:30,
+	padding:12,
+	columns:12
 },
 wvga : {
 	grid:748,
 	short:'t',
 	long:'tablet',
-	res:'WVGA'
+	res:'WVGA',
+	module:44,
+	gutter:20,
+	padding:10,
+	columns:12
 },
 hvga : {
 	grid:460,
 	short:'l',
 	long:'landscape',
-	res:'HVGA'
+	res:'HVGA',
+	module:40,
+	gutter:20,
+	padding:10,
+	columns:8
 },
 qvga : {
 	grid:300,
 	short:'m',
 	long:'mobile',
-	res:'QVGA'
+	res:'QVGA',
+	module:27,
+	gutter:12,
+	padding:10,
+	columns:8
 }
 };
 
-function genGrids(opt){
-var ret = '\n'+
-'// ' + opt.grid + '\n'+
-'// props'+ '\n'+
-'.g-grid(@a, ' + opt.grid + ') when (isnumber(@a)) {'+ '\n'+
-'	.g-grid-props(@a, @width-' + opt.grid + ', @module-' + opt.grid + ', @gutter-' + opt.grid + ', false);'+ '\n'+
-'}'+ '\n'+
-'.g-grid(@a, ' + opt.grid + ', right) when (isnumber(@a)) {'+ '\n'+
-'	.g-grid-props(@a, @width-' + opt.grid + ', @module-' + opt.grid + ', @gutter-' + opt.grid + ', false, right);'+ '\n'+
-'}'+ '\n'+
-'.g-grid(all, ' + opt.grid + ') {'+ '\n'+
-'	.g-grid-props(@columns-' + opt.grid + ', @width-' + opt.grid + ', @module-' + opt.grid + ', @gutter-' + opt.grid + ', false);'+ '\n'+
-'}'+ '\n'+
-'.g-grid(padding, ' + opt.grid + ') {'+ '\n'+
-'	.g-grid-padding(@padding-' + opt.grid + ');'+ '\n'+
-'}'+ '\n'+
-'.g-grid(padding, ' + opt.grid + ', fluid) {'+ '\n'+
-'	.g-grid-padding(@padding-' + opt.grid + '-fluid);'+ '\n'+
-'}'+ '\n'+
-'.g-grid(@a, ' + opt.grid + ', only) when (isnumber(@a)) {'+ '\n'+
-'	.g-grid-width(@a,@module-' + opt.grid + ',@gutter-' + opt.grid + ');'+ '\n'+
-'}'+ '\n'+
-'.g-grid(@a, ' + opt.grid + ', lead) when (isnumber(@a)) {'+ '\n'+
-'	.g-grid-props(@a, @width-' + opt.grid + ', @module-' + opt.grid + ', @gutter-' + opt.grid + ', lead);'+ '\n'+
-'}'+ '\n'+
-'.g-grid(@a, ' + opt.grid + ', lead, right) when (isnumber(@a)) {'+ '\n'+
-'	.g-grid-props(@a, @width-' + opt.grid + ', @module-' + opt.grid + ', @gutter-' + opt.grid + ', lead, right);'+ '\n'+
-'}'+ '\n'+
-'// fluid'+ '\n'+
-'.g-grid(all, ' + opt.grid + ', fluid){'+ '\n'+
-'	.g-grid-props(100%, 100%, @module-' + opt.grid + '-fluid, @gutter-' + opt.grid + '-fluid, false);'+ '\n'+
-'}'+ '\n'+
-'.g-grid(@a, ' + opt.grid + ', fluid) when (isnumber(@a)) {'+ '\n'+
-'	.g-grid-props(@a, 100%, @module-' + opt.grid + '-fluid, @gutter-' + opt.grid + '-fluid, false);'+ '\n'+
-'}'+ '\n'+
-'.g-grid(@a, ' + opt.grid + ', fluid, right) when (isnumber(@a)) {'+ '\n'+
-'	.g-grid-props(@a, 100%, @module-' + opt.grid + '-fluid, @gutter-' + opt.grid + '-fluid, false, right);'+ '\n'+
-'}'+ '\n'+
-'.g-grid(@a, ' + opt.grid + ', fluid, only) when (isnumber(@a)) {'+ '\n'+
-'	.g-grid-width(@a, @module-' + opt.grid + '-fluid, @gutter-' + opt.grid + '-fluid);'+ '\n'+
-'}'+ '\n'+
-'.g-grid(@a, ' + opt.grid + ', fluid, lead) when (isnumber(@a)) {'+ '\n'+
-'	.g-grid-props(@a, 100%, @module-' + opt.grid + '-fluid, @gutter-' + opt.grid + '-fluid, lead);'+ '\n'+
-'}'+ '\n'+
-'.g-grid(@a, ' + opt.grid + ', fluid, lead, right) when (isnumber(@a)) {'+ '\n'+
-'	.g-grid-props(@a, 100%, @module-' + opt.grid + '-fluid, @gutter-' + opt.grid + '-fluid, lead, right);'+ '\n'+
-'}'+ '\n'+
-''+ '\n'+
-'// ' + opt.short + '\n'+
-'.g-grid(@a, ' + opt.short + ') when not (@a = a)  and not (@a = p) { .g-grid(@a, ' + opt.grid + '); }'+ '\n'+
-'.g-grid(@a, ' + opt.short + ', @b) when not (@a = p) and not (@a = a) and not (@b = l) and not (@b = left) and not (@b = f) and not (@b = flex) and not (@b = o) and not (@b = r) { .g-grid(@a, ' + opt.grid + ', @b); }'+ '\n'+
-'.g-grid(@a, ' + opt.short + ', @b, @c) when not (@b = l) and not (@b = left) and not (@b = f) and not (@b = flex) and not (@b = o) and not (@b = r) and not (@c = l) and not (@c = left) and not (@c = f) and not (@c = flex) and not (@c = o) and not (@c = r) { .g-grid(@a, ' + opt.grid + ', @b, @c); }'+ '\n'+
-'.g-grid(@a, ' + opt.short + ', @b, @c, @d) when not (@b = l) and not (@b = left) and not (@b = f) and not (@b = flex) and not (@b = o) and not (@b = r) and not (@c = l) and not (@c = left) and not (@c = f) and not (@c = flex) and not (@c = o) and not (@c = r) and not (@d = l) and not (@d = left) and not (@d = f) and not (@d = flex) and not (@d = r) and not (@d = o) { .g-grid(@a, ' + opt.grid + ', @b, @c, @d); }'+ '\n'+
-''+ '\n'+
-'// ' + opt.long + '\n'+
-'.g-grid(@a, ' + opt.long + ') when not (@a = a)  and not (@a = p) { .g-grid(@a, ' + opt.grid + '); }'+ '\n'+
-'.g-grid(@a, ' + opt.long + ', @b) when not (@a = p) and not (@a = a) and not (@b = l) and not (@b = left) and not (@b = f) and not (@b = flex) and not (@b = o) and not (@b = r) { .g-grid(@a, ' + opt.grid + ', @b); }'+ '\n'+
-'.g-grid(@a, ' + opt.long + ', @b, @c) when not (@b = l) and not (@b = left) and not (@b = f) and not (@b = flex) and not (@b = o) and not (@b = r) and not (@c = l) and not (@c = left) and not (@c = f) and not (@c = flex) and not (@c = o) and not (@c = r) { .g-grid(@a, ' + opt.grid + ', @b, @c); }'+ '\n'+
-'.g-grid(@a, ' + opt.long + ', @b, @c, @d) when not (@b = l) and not (@b = left) and not (@b = f) and not (@b = flex) and not (@b = o) and not (@b = r) and not (@c = l) and not (@c = left) and not (@c = f) and not (@c = flex) and not (@c = o) and not (@c = r) and not (@d = l) and not (@d = left) and not (@d = f) and not (@d = flex) and not (@d = r) and not (@d = o) { .g-grid(@a, ' + opt.grid + ', @b, @c, @d); }'+ '\n'+
-''+ '\n'+
-'// ' + opt.res + '\n'+
-'.g-grid(@a, ' + opt.res + ') when not (@a = a)  and not (@a = p) { .g-grid(@a, ' + opt.grid + '); }'+ '\n'+
-'.g-grid(@a, ' + opt.res + ', @b) when not (@a = p) and not (@a = a) and not (@b = l) and not (@b = left) and not (@b = f) and not (@b = flex) and not (@b = o) and not (@b = r) { .g-grid(@a, ' + opt.grid + ', @b); }'+ '\n'+
-'.g-grid(@a, ' + opt.res + ', @b, @c) when not (@b = l) and not (@b = left) and not (@b = f) and not (@b = flex) and not (@b = o) and not (@b = r) and not (@c = l) and not (@c = left) and not (@c = f) and not (@c = flex) and not (@c = o) and not (@c = r) { .g-grid(@a, ' + opt.grid + ', @b, @c); }'+ '\n'+
-'.g-grid(@a, ' + opt.res + ', @b, @c, @d) when not (@b = l) and not (@b = left) and not (@b = f) and not (@b = flex) and not (@b = o) and not (@b = r) and not (@c = l) and not (@c = left) and not (@c = f) and not (@c = flex) and not (@c = o) and not (@c = r) and not (@d = l) and not (@d = left) and not (@d = f) and not (@d = flex) and not (@d = r) and not (@d = o) { .g-grid(@a, ' + opt.grid + ', @b, @c, @d); }' + '\n';
-return ret;
-};
+var shortHand = function(gS, gL){
+	var first = ['a','all','@b'],
+		firstFull = ['all','all','@b'],
+		firstL = first.length,
+		second = ['f','flex','fluid','l','left','lead','r','reverse','right'],
+		secondFull = ['fluid','fluid','fluid','lead','lead','lead','right','right','right'],
+		secondL = second.length,
+		third = ['l','left','lead','r','reverse','right'],
+		thirdFull = ['lead','lead','lead','right','right','right'],
+		thirdL = third.length,
+		fourth = ['r','reverse','right'],
+		fourthFull = ['right','right','right'],
+		fourthL = fourth.length,
+		sH = '.g-grid(' + gS + ',',
+		lH = '.g-grid(' + gL + ',',
+		isGrid = sH == lH;
+		sR = '';
 
-function pop(){
-	for (sys in grids){
-		toWrite += genGrids(grids[sys]);
+	var gridCheck = function(first,firstF,second,secondF,third,thirdF,fourth,fourthF){
+		var notShort = false;
+		if(fourth != 'undefined'){
+			if(first == firstF && second == secondF && third == thirdF && fourth == fourthF){
+				notShort = true;
+			};
+		}else if(third != 'undefined'){
+			if(first == firstF && second == secondF && third == thirdF){
+				notShort = true;
+			};
+		}else if(second != 'undefined'){
+			if(first == firstF && second == secondF){
+				notShort = true;
+			};
+		}else{
+			if(first == firstF){
+				notShort = true;
+			};
+		}
+		return notShort;
+	};
+
+	for(i=0;i<firstL;i++){
+		var isNum = (firstFull[i] == '@b') ? ' when (isnumber(@b))' : '';
+		if(!(isGrid && gridCheck(first[i],firstFull[i]))){
+			sR = sR + sH + first[i] + ')' + isNum + '{' + lH + firstFull[i] + ');}\n';
+		}
+		for(j=0;j<secondL;j++){
+			if(firstFull[i] == 'all' && secondFull[j] == 'fluid' || firstFull[i] == '@b'){
+				if(!(isGrid && gridCheck(first[i],firstFull[i],second[j],secondFull[j]))){
+					sR = sR + sH + first[i] + ',' +  second[j] + ')' + isNum + '{' + lH + firstFull[i] + ',' + secondFull[j] + ');}\n';
+				}
+				for(k=0;k<thirdL;k++){
+					if(secondFull[j] != thirdFull[k] && secondFull[j] != 'right' && firstFull[i] != 'all'){
+						if(!(isGrid && gridCheck(first[i],firstFull[i],second[j],secondFull[j],third[k],thirdFull[k]))){
+							sR = sR + sH + first[i] + ',' +  second[j] + ',' +  third[k] + ')' + isNum + '{' + lH + firstFull[i] + ',' + secondFull[j] + ',' + thirdFull[k] + ');}\n';
+						}
+						for(l=0;l<fourthL;l++){
+							if(thirdFull[k] != 'right'){
+								if(!(isGrid && gridCheck(first[i],firstFull[i],second[j],secondFull[j],third[k],thirdFull[k],fourth[l],fourthFull[l]))){
+									sR = sR + sH + first[i] + ',' +  second[j] + ',' +  third[k] + ',' +  fourth[l] + ')' + isNum + '{' + lH + firstFull[i] + ',' + secondFull[j] + ',' + thirdFull[k] + ',' + fourthFull[l] +  ');}\n';
+								}
+							}
+						}
+					}
+				}
+			}
+		}
 	}
-	return toWrite;
+	return sR;
 };
 
-fs.writeFile('g-grid.less', pop(), 'ascii')
+function genGrids(opt){
+	var fluidMod = 100*(opt.module/opt.grid),
+		fluidGut = 100*(opt.gutter/opt.grid),
+		fluidPad = 100*(opt.padding/(opt.grid + (opt.padding * 2)));
+
+	// vars
+	var gGridCore = '\n'+
+		'// ' + opt.grid + '\n'+
+		'// vars'+ '\n'+
+		'@g-grid-' + opt.grid + '-width:' + opt.grid + 'px;' + '\n'+
+		'@g-grid-' + opt.grid + '-module:' + opt.module + 'px;' + '\n'+
+		'@g-grid-' + opt.grid + '-gutter:' + opt.gutter + 'px;' + '\n'+
+		'@g-grid-' + opt.grid + '-padding:' + opt.padding + 'px;' + '\n'+
+		'@g-grid-' + opt.grid + '-columns:' + opt.columns + ';' + '\n'+
+		'// fluid vars'+ '\n'+
+		'@g-grid-' + opt.grid + '-module-fluid:' + fluidMod + '%;' + '\n'+
+		'@g-grid-' + opt.grid + '-gutter-fluid:' + fluidGut + '%;' + '\n'+
+		'@g-grid-' + opt.grid + '-padding-fluid:' + fluidPad + '%;' + '\n';
+
+	// props
+	gGridCore += '// props'+ '\n'+
+		'.g-grid(' + opt.grid + ', @a) when (isnumber(@a)) {'+ '\n'+
+		'	.g-grid-props(@a, @g-grid-' + opt.grid + '-width, @g-grid-' + opt.grid + '-module, @g-grid-' + opt.grid + '-gutter, false);'+ '\n'+
+		'}'+ '\n'+
+		'.g-grid(' + opt.grid + ', @a, right) when (isnumber(@a)) {'+ '\n'+
+		'	.g-grid-props(@a, @g-grid-' + opt.grid + '-width, @g-grid-' + opt.grid + '-module, @g-grid-' + opt.grid + '-gutter, false, right);'+ '\n'+
+		'}'+ '\n'+
+		'.g-grid(' + opt.grid + ', @a, lead) when (isnumber(@a)) {'+ '\n'+
+		'	.g-grid-props(@a, @g-grid-' + opt.grid + '-width, @g-grid-' + opt.grid + '-module, @g-grid-' + opt.grid + '-gutter, lead);'+ '\n'+
+		'}'+ '\n'+
+		'.g-grid(' + opt.grid + ', @a, lead, right) when (isnumber(@a)) {'+ '\n'+
+		'	.g-grid-props(@a, @g-grid-' + opt.grid + '-width, @g-grid-' + opt.grid + '-module, @g-grid-' + opt.grid + '-gutter, lead, right);'+ '\n'+
+		'}'+ '\n'+
+		'// fluid'+ '\n'+
+		'.g-grid(' + opt.grid + ', @a, fluid) when (isnumber(@a)) {'+ '\n'+
+		'	.g-grid-props(@a, 100%, @g-grid-' + opt.grid + '-module-fluid, @g-grid-' + opt.grid + '-gutter-fluid, false);'+ '\n'+
+		'}'+ '\n'+
+		'.g-grid(' + opt.grid + ', @a, fluid, right) when (isnumber(@a)) {'+ '\n'+
+		'	.g-grid-props(@a, 100%, @g-grid-' + opt.grid + '-module-fluid, @g-grid-' + opt.grid + '-gutter-fluid, false, right);'+ '\n'+
+		'}'+ '\n'+
+		'.g-grid(' + opt.grid + ', @a, fluid, lead) when (isnumber(@a)) {'+ '\n'+
+		'	.g-grid-props(@a, 100%, @g-grid-' + opt.grid + '-module-fluid, @g-grid-' + opt.grid + '-gutter-fluid, lead);'+ '\n'+
+		'}'+ '\n'+
+		'.g-grid(' + opt.grid + ', @a, fluid, lead, right) when (isnumber(@a)) {'+ '\n'+
+		'	.g-grid-props(@a, 100%, @g-grid-' + opt.grid + '-module-fluid, @g-grid-' + opt.grid + '-gutter-fluid, lead, right);'+ '\n'+
+		'}'+ '\n'+
+		'// all'+ '\n'+
+		'.g-grid(' + opt.grid + ', all) {'+ '\n'+
+		'	.g-grid-props(@g-grid-' + opt.grid + '-columns, @g-grid-' + opt.grid + '-width, @g-grid-' + opt.grid + '-module, @g-grid-' + opt.grid + '-gutter, false);'+ '\n'+
+		'}'+ '\n'+
+		'.g-grid(' + opt.grid + ', all, fluid){'+ '\n'+
+		'	.g-grid-props(100%, 100%, @g-grid-' + opt.grid + '-module-fluid, @g-grid-' + opt.grid + '-gutter-fluid, false);'+ '\n'+
+		'}'+ '\n';
+
+	// shorthand
+	gGridCore += '// shorthand' + '\n';
+	gGridCore += shortHand(opt.grid, opt.grid);
+	gGridCore += shortHand(opt.short, opt.grid);
+	gGridCore += shortHand(opt.long, opt.grid);
+	gGridCore += shortHand(opt.res, opt.grid);
+
+	gGridCore += '// widths' + '\n';
+	for(var i = 1; i <= opt.columns; i++){
+		gGridCore += '@g-grid-' + opt.grid + '-' + i + ':' + ( opt.module * i + opt.gutter * (i - 1)) + 'px;\n' +
+		'@g-grid-' + opt.grid + '-' + i + '-fluid:' + ( fluidMod * i + fluidGut * (i - 1)) + '%;\n';
+		if(i == opt.columns){
+			gGridCore += '@g-grid-' + opt.grid + '-all:' + ( opt.module * i + opt.gutter * (i - 1)) + 'px;\n' +
+			'@g-grid-' + opt.grid + '-all-fluid:' + ( fluidMod * i + fluidGut * (i - 1)) + '%;\n';
+		}
+	}
+
+	return gGridCore;
+};
+
+fs.readFile('logo.txt', 'ascii', function(err,data){
+	gGrid += data;
+	fs.readFile('../LICENSE', 'ascii', function(err,data){
+		gGrid += '\n/*\n' + data + '\n*/\n';
+		for (sys in grids){
+			gGrid += genGrids(grids[sys]);
+		}
+		fs.readFile('props.less', 'ascii', function(err,data){
+			gGrid += data;
+			fs.writeFile('g-grid.less', gGrid, 'ascii');
+		});
+	});
+});
+
